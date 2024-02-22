d96d821ca2a482493fb0f142a35025e5fbafca45
brklntmhwk <to.be.wanryu@gmail.com>
2024-02-22 18:18:38 +0900

    consts ---> content/meta/site-data.yml

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,29 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const meta = defineCollection({
+  type: "data",
+  schema: z.object({
+    site: z.object({
+      title: z.string(),
+      description: z.string(),
+    }),
+    blog: z.object({
+      description: z.string(),
+    }),
+  }),
+});
+
+export const collections = { blog, meta };
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,17 @@
 import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { getEntry } from "astro:content";
+
+const meta = await getEntry("meta", "site-data");
+
+// import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context: APIContext) {
   const posts = await getCollection("blog");
   return await rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: meta.data.site.title,
+    description: meta.data.site.description,
     site: context.site ?? "",
     items: posts.map((post) => ({
       ...post.data,

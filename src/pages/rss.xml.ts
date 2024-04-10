4f486f3b890e1a9cebef4dbeeb1178b0c8543b4e
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-04-10 11:54:18 +0900

    rss feed tweaked according to internationalization

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,20 +2,26 @@ import rss from '@astrojs/rss'
 import type { APIContext } from 'astro'
 import { getCollection } from 'astro:content'
 import { getEntry } from 'astro:content'
-
-const meta = await getEntry('meta', 'site-data')
+import { langList } from '@/i18n/ui'
 
 export async function GET(context: APIContext) {
-  const posts = await getCollection('blog')
+  const entries = await getCollection('blog')
 
-  return await rss({
-    title: meta.data.site.title,
-    description: meta.data.site.description,
-    site: context.site ?? '',
-    items: posts.map((post) => ({
-      ...post.data,
-      pubDate: post.data.publishedAt,
-      link: `/blog/${post.slug}/`,
-    })),
+  langList.map(async (lang) => {
+    const meta = await getEntry('meta', `${lang}/site-data`)
+    const localeEntries = entries.filter(
+      (entry) => entry.slug.split('/')[0] === lang
+    )
+
+    return await rss({
+      title: meta.data.site.title,
+      description: meta.data.site.description,
+      site: context.site ?? '',
+      items: localeEntries.map((entry) => ({
+        ...entry.data,
+        pubDate: entry.data.publishedAt,
+        link: `/${lang}/blog/${entry.slug}/`,
+      })),
+    })
   })
 }

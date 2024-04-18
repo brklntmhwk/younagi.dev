4924c771e0e13dcdd609efd316b4465f72a9420e
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-04-18 15:18:51 +0900

    ogImage settings (astro-og-canvas)

--- src/pages/og/[...slug].png.ts
+++ src/pages/og/[...slug].png.ts
@@ -4,11 +4,12 @@ import { OGImageRoute } from 'astro-og-canvas'
 const entries = await getCollection('blog')
 // const pages = await getCollection('page')
 const articles = Object.fromEntries(
-  entries.map(({ slug, data, collection }) => {
-    const locale = slug.slice(0, slug.indexOf('/'))
+  entries.map(({ slug, data /* , collection */ }) => {
+    // const locale = slug.slice(0, slug.indexOf('/'))
     const rawSlug = slug.slice(slug.indexOf('/') + 1)
 
-    return [`${locale}/${collection}/${rawSlug}`, data]
+    return [rawSlug, data]
+    // return [`${locale}/${collection}/${rawSlug}`, data]
   })
 )
 // const articles = Object.fromEntries(
@@ -33,42 +34,42 @@ export const { getStaticPaths, GET } = OGImageRoute({
       description: page.description ?? '',
       bgGradient: [[47, 28, 66]],
       // border: { color: [227, 182, 237], width: 10, side: 'block-end' },
-      font: {
-        title: {
-          size: 72,
-          lineHeight: 1.5,
-          families: [
-            'PixelMPlus10',
-            'Noto Sans',
-            'Noto Sans Arabic',
-            'Noto Sans SC',
-            'Noto Sans TC',
-            'Noto Sans JP',
-            'Noto Sans KR',
-          ],
-          weight: 'Medium',
-          color: [255, 255, 255],
-        },
-        description: {
-          size: 42,
-          lineHeight: 1.2,
-          families: [
-            'DotGothic16',
-            'Noto Sans',
-            'Noto Sans SC',
-            'Noto Sans JP',
-          ],
-          weight: 'Normal',
-          color: [255, 255, 255],
-        },
-      },
+      // font: {
+      //   title: {
+      //     size: 72,
+      //     lineHeight: 1.5,
+      //     families: [
+      //       'PixelMPlus10',
+      //       'Noto Sans',
+      //       'Noto Sans Arabic',
+      //       'Noto Sans SC',
+      //       'Noto Sans TC',
+      //       'Noto Sans JP',
+      //       'Noto Sans KR',
+      //     ],
+      //     weight: 'Medium',
+      //     color: [255, 255, 255],
+      //   },
+      //   description: {
+      //     size: 42,
+      //     lineHeight: 1.2,
+      //     families: [
+      //       'DotGothic16',
+      //       'Noto Sans',
+      //       'Noto Sans SC',
+      //       'Noto Sans JP',
+      //     ],
+      //     weight: 'Normal',
+      //     color: [255, 255, 255],
+      //   },
+      // },
       // bgImage: {
       //   path: './src/assets/images/hitokage.png',
       // },
-      fonts: [
-        './src/assets/fonts/PixelMplus10-Regular.woff2',
-        './src/assets/fonts/DotGothic16-Regular.woff2',
-      ],
+      // fonts: [
+      //   './src/assets/fonts/PixelMplus10-Regular.woff2',
+      //   './src/assets/fonts/DotGothic16-Regular.woff2',
+      // ],
     }
   },
 })

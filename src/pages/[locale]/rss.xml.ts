2d61cbbc7dbe25dc59fe065135664eb8ce6dda25
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-04-19 17:57:24 +0900

    rss feed internationalized

--- src/pages/[locale]/rss.xml.ts
+++ src/pages/[locale]/rss.xml.ts
@@ -4,15 +4,15 @@ import { getCollection } from 'astro:content'
 import { getEntry } from 'astro:content'
 import { langList } from '@/i18n/data'
 
-export const getStaticPaths = () => {
+export const getStaticPaths = async () => {
   return langList.map((lang) => ({ params: { locale: lang } }))
 }
 
 export async function GET(context: APIContext) {
   const localeParam = context.params.locale
-  const locale = langList.find((lang) => lang === localeParam)!
+  const locale = langList.find((lang) => lang === localeParam)
   const entries = await getCollection('blog')
-  const meta = await getEntry('meta', `${locale}/site-data`)
+  const meta = await getEntry('meta', `${locale ?? 'en'}/site-data`)
 
   return await rss({
     title: meta.data.site.title,

c1fe401894565994757efca42f066c1dc779eba5
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-04-23 16:09:15 +0900

    metadata tweaked

--- astro.config.ts
+++ astro.config.ts
@@ -79,7 +79,7 @@ export default defineConfig({
         {
           theme: {
             light: 'github-dark',
-            dark: 'github-light',
+            dark: 'github-dark',
           },
           grid: false,
         },

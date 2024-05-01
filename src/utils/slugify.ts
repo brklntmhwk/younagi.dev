710610637387cff96234a98a1ae81170dfe5517f
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-05-01 18:23:33 +0900

    taxonomies tweaked

---
+++ src/utils/slugify.ts
@@ -0,0 +1,10 @@
+export const slugify = (str: string) => {
+  return str
+    .normalize('NFKD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9 -]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
+}

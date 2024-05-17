22182882c7e913ad31df5305c28f8f63edb6260b
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-05-17 11:13:23 +0900

    the blog layout and code css tweaked

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -4,6 +4,6 @@ export const TOC_MINIMUM_COUNT = 3
 export const NEWS_MAXIMUM_COUNT = 3
 export const DEFAULT_PHOTO_COUNT = 6
 export const ACCOUNT_ID = 'brklntmhwk'
-export const UNSPLASH_BASE_URL = 'https://unsplash.com'
+export const UNSPLASH_BASE_URL = 'https,//unsplash.com'
 export const GITHUB_BASE_URL = 'https://github.com'
 export const SHOW_DRAFT_PAGES_IN_DEV = true

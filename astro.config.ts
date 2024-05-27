6d099bcb5aa7d02fa4e27ba0cf39550dc047adb9
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-05-27 11:09:32 +0900

    cloudflare turntile added to Contact page and cloudflare adapter installed

--- astro.config.ts
+++ astro.config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
       },
     },
     ssr: {
-      external: ['node:fs/promises', 'node:path'],
+      external: ['node:fs/promises', 'node:path', 'node:crypto'],
     },
   },
   markdown: {

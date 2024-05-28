6368d9848d77a79ef97e5d05d46facca3248d384
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-05-28 12:49:56 +0900

    cf worker api path and code tweaked

--- functions/submitForm.ts
+++ functions/submitForm.ts
@@ -79,14 +79,38 @@ export default {
     } catch (error) {
       return new Response(
         JSON.stringify({
-          message: 'Internal server error',
-        }),
-        {
-          status: 500,
-        }
+          message: getErrorMessage(error),
+        })
       )
     }
   },
+}
+
+type ErrorWithMessage = {
+  message: string
+}
+
+function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'message' in error &&
+    typeof (error as Record<string, unknown>).message === 'string'
+  )
+}
+
+function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
+  if (isErrorWithMessage(maybeError)) return maybeError
+
+  try {
+    return new Error(JSON.stringify(maybeError))
+  } catch {
+    return new Error(String(maybeError))
+  }
+}
+
+function getErrorMessage(error: unknown) {
+  return toErrorWithMessage(error).message
 }
 
 // export const handleRequest = async (event: FetchEvent): Promise<Response> => {

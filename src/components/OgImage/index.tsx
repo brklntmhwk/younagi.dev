f5cf68615946ded7d0f25085bb8fda23211119b1
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-05-29 17:16:28 +0900

    OGImage: satori HTML --> Object literal form

--- src/components/OgImage/index.tsx
+++ src/components/OgImage/index.tsx
@@ -1,7 +1,4 @@
-/** @jsxImportSource react */
-/** @jsxRuntime automatic */
-
-import { readFileSync } from 'fs'
+import { readFileSync } from 'node:fs'
 import satori from 'satori'
 import sharp from 'sharp'
 
@@ -13,7 +10,7 @@ const OgImage = async (text: string) => {
   //   './node_modules/@fontsource-variable/inconsolata/files/inconsolata-latin-ext-wdth-normal.woff2'
   // )
   const logoBuffer = readFileSync('./src/assets/images/logo.png')
-  const icon = btoa(
+  const logo = btoa(
     new Uint8Array(logoBuffer).reduce(
       (data, byte) => data + String.fromCharCode(byte),
       ''
@@ -21,108 +18,72 @@ const OgImage = async (text: string) => {
   )
 
   const svg = await satori(
-    <div
-      style={{
-        fontFamily: 'PixelMPlus10, sans-serif',
-        backgroundColor: '#120e12',
-        color: '#f2f0e6',
-        display: 'flex',
-        flexDirection: 'row',
-        height: '100%',
-        width: '100%',
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: 'white',
-          color: '#120e12',
+    {
+      type: 'div',
+      props: {
+        style: {
+          fontFamily: 'PixelMPlus10, sans-serif',
+          backgroundColor: '#f7f7f7',
+          color: '#1c1b22',
           display: 'flex',
           flexDirection: 'column',
-          width: '30%',
-          padding: '2.5rem',
-          justifyContent: 'center',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '1rem',
-          }}
-        >
-          <h2
-            style={{
-              fontSize: '17rem',
+          gap: '1.5rem',
+          padding: '1.85rem',
+          justifyContent: 'space-between',
+        },
+      },
+      children: [
+        {
+          type: 'h2',
+          props: {
+            style: {
+              fontSize: '1.5rem',
+              fontWeight: 700,
+            },
+          },
+          children: text,
+          key: 'Page title',
+        },
+        {
+          type: 'div',
+          props: {
+            style: {
               display: 'flex',
               alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            sssss
-          </h2>
-        </div>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '70%',
-          maxHeight: '100%',
-          padding: '3rem',
-        }}
-      >
-        <h1
-          style={{
-            marginTop: 0,
-            fontSize: '4rem',
-            width: '100%',
-            flexGrow: 1,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            lineHeight: '1.2',
-          }}
-        >
-          {text}
-        </h1>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            width: '100%',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            <img
-              src={`data:image/jpeg;base64,${icon}`}
-              alt="younagi.dev icon"
-              width={80}
-              height={80}
-              style={{
-                borderRadius: '9999px',
-                marginRight: '1.25rem',
-              }}
-            />
-            <h2
-              style={{
-                marginRight: '1.25rem',
-                fontFamily: 'PixelMPlus10',
-                fontSize: '2.5rem',
-              }}
-            >
-              {'Nagi (凪)'}
-            </h2>
-          </div>
-        </div>
-      </div>
-    </div>,
+              gap: '1.5rem',
+            },
+          },
+          children: [
+            {
+              type: 'img',
+              props: {
+                src: `data:image/png;base64,${logo}`,
+                alt: 'younagi.dev site logo',
+                width: '80',
+                height: '80',
+                style: {
+                  borderRadius: '9999px',
+                },
+              },
+              children: [],
+              key: 'Site logo image',
+            },
+            {
+              type: 'span',
+              props: {
+                style: {
+                  fontSize: '1.25rem',
+                },
+              },
+              children: 'younagi.dev',
+              key: 'Site name',
+            },
+          ],
+          key: '',
+        },
+      ],
+      key: '',
+    },
     {
       width: 1200,
       height: 630,

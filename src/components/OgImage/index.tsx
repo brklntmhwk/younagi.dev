33c9b43610b1acfd35cb9c37206866e291351d79
brklntmhwk_w <to.be.wanryu@gmail.com>
2024-06-27 18:42:55 +0900

    OGImage not showing addressed

--- src/components/OgImage/index.tsx
+++ src/components/OgImage/index.tsx
@@ -9,28 +9,98 @@ const fontPixelMPlus10Regular = readFileSync(
 const fontPixelMPlus10Bold = readFileSync(
   './src/assets/fonts/PixelMplus10-Bold.woff'
 )
-// const backgroundImage = readFileSync('./src/assets/images/og-bg.jpg').toString(
-//   'base64'
-// )
-// const logoBuffer = readFileSync('./src/assets/images/logo.png')
-// const logo = btoa(
-//   new Uint8Array(logoBuffer).reduce(
-//     (data, byte) => data + String.fromCharCode(byte),
-//     ''
-//   )
-// )
+const backgroundImage = readFileSync('./src/assets/images/og-bg.jpg').toString(
+  'base64'
+)
+const logoBuffer = readFileSync('./src/assets/images/logo.png')
+const logo = btoa(
+  new Uint8Array(logoBuffer).reduce(
+    (data, byte) => data + String.fromCharCode(byte),
+    ''
+  )
+)
 
-export const getOgImage = async (text: string, t: I18nData<'og_image'>) => {
+const getOgImage = async (text: string, t: I18nData<'og_image'>) => {
   const svg = await satori(
     {
       type: 'div',
       props: {
         style: {
-          backgroundColor: t.logo_alt,
+          fontFamily: 'PixelMPlus10, sans-serif',
+          backgroundImage: `url(data:image/jpeg;base64,${backgroundImage})`,
+          backgroundSize: '1200px 630px',
+          width: 1200,
+          height: 630,
+          display: 'flex',
         },
-        children: `Text: ${text}`,
+        children: [
+          {
+            type: 'div',
+            props: {
+              style: {
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '1.5rem',
+                padding: '2.25rem',
+              },
+              children: [
+                {
+                  type: 'h2',
+                  props: {
+                    style: {
+                      color: '#f7f7f7',
+                      fontSize: '2.875rem',
+                      fontWeight: '700',
+                      lineHeight: '1.25',
+                    },
+                    children: `${text}`,
+                  },
+                  key: 'Page title',
+                },
+                {
+                  type: 'div',
+                  props: {
+                    style: {
+                      display: 'flex',
+                      alignItems: 'center',
+                      gap: '1.5rem',
+                    },
+                    children: [
+                      {
+                        type: 'img',
+                        props: {
+                          src: `data:image/png;base64,${logo}`,
+                          alt: t.logo_alt,
+                          width: '60',
+                          height: '60',
+                          style: {
+                            borderRadius: '9999px',
+                          },
+                        },
+                        key: 'Site logo image',
+                      },
+                      {
+                        type: 'span',
+                        props: {
+                          style: {
+                            fontSize: '2.25rem',
+                            color: '#1c1b22',
+                          },
+                          children: t.site_name,
+                        },
+                        key: 'Site name',
+                      },
+                    ],
+                  },
+                  key: 'Image container',
+                },
+              ],
+            },
+            key: 'OG image second wrapper',
+          },
+        ],
       },
-      key: 'div tag',
+      key: 'OG image outermost wrapper',
     },
     {
       width: 1200,
@@ -60,83 +130,5 @@ export const getOgImage = async (text: string, t: I18nData<'og_image'>) => {
 
   return imgBuffer
 }
-// {
-//       type: 'div',
-//       props: {
-//         style: {
-//           fontFamily: 'PixelMPlus10, sans-serif',
-//           backgroundImage: `url(data:image/jpeg;base64,${backgroundImage})`,
-//           backgroundSize: '1200px 630px',
-//           width: 1200,
-//           height: 630,
-//           display: 'flex',
-//         },
-//         children: [
-//           {
-//             type: 'div',
-//             props: {
-//               style: {
-//                 display: 'flex',
-//                 flexDirection: 'column',
-//                 gap: '1.5rem',
-//                 padding: '2.25rem',
-//               },
-//               children: [
-//                 {
-//                   type: 'h2',
-//                   props: {
-//                     style: {
-//                       color: '#f7f7f7',
-//                       fontSize: '2.875rem',
-//                       fontWeight: '700',
-//                       lineHeight: '1.25',
-//                     },
-//                     children: `${text}`,
-//                   },
-//                   key: 'Page title',
-//                 },
-//                 {
-//                   type: 'div',
-//                   props: {
-//                     style: {
-//                       display: 'flex',
-//                       alignItems: 'center',
-//                       gap: '1.5rem',
-//                     },
-//                     children: [
-//                       {
-//                         type: 'img',
-//                         props: {
-//                           src: `data:image/png;base64,${logo}`,
-//                           alt: t.logo_alt,
-//                           width: '60',
-//                           height: '60',
-//                           style: {
-//                             borderRadius: '9999px',
-//                           },
-//                         },
-//                         key: 'Site logo image',
-//                       },
-//                       {
-//                         type: 'span',
-//                         props: {
-//                           style: {
-//                             fontSize: '2.25rem',
-//                             color: '#1c1b22',
-//                           },
-//                           children: t.site_name,
-//                         },
-//                         key: 'Site name',
-//                       },
-//                     ],
-//                   },
-//                   key: 'Image container',
-//                 },
-//               ],
-//             },
-//             key: 'OG image second wrapper',
-//           },
-//         ],
-//       },
-//       key: 'OG image outermost wrapper',
-//     },
+
+export default getOgImage

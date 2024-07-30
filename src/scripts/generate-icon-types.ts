03ba1d9aba2dc4a0119a1915784b92ec61127000
brklntmhwk_dev <86272619+brklntmhwk@users.noreply.github.com>
2024-07-30 08:08:51 +0000

    refactor(types): ✨ remove generate icon types script

--- src/scripts/generate-icon-types.ts
+++ src/scripts/generate-icon-types.ts
@@ -4,12 +4,12 @@ import { resolve } from 'node:path';
 const ICONS_DIR = '../components/Icon/icons';
 const ICON_TYPES_DIR = '../components/Icon/types.ts';
 
-const iconsDir = resolve(__dirname, ICONS_DIR);
+const iconsDir = resolve(import.meta.dirname, ICONS_DIR);
 console.log(iconsDir);
 const iconFiles = readdirSync(iconsDir);
 const iconNames = iconFiles.map((file) => file.replace('.svg', ''));
 
 const typeDef = `export type IconName = ${iconNames.map((name) => `'${name}'`).join(' | ')};`;
 
-writeFileSync(resolve(__dirname, ICON_TYPES_DIR), typeDef);
+writeFileSync(resolve(import.meta.dirname, ICON_TYPES_DIR), typeDef);
 console.log(`SVG Icon types has been generated in ${iconsDir}.`);

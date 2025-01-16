2f24e1deb572bda88c5597daa54388e763c8ed17
brklntmhwk_dev <86272619+brklntmhwk@users.noreply.github.com>
2025-01-16 07:10:08 +0000

    style(ui): 💄 tweak search style

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -174,14 +174,16 @@ export const Search: Component<Props> = (props) => {
         </div>
         <input
           type="reset"
-          class="font-semibold cursor-pointer py-1 px-2 rounded-sm self-center border-2 border-solid border-line-solid hover:underline hover:underline-offset-4"
+          class="font-semibold cursor-pointer py-1 px-2 rounded-sm self-center border-2 border-solid border-line-solid hover:bg-default-reverse-hover"
           value={props.t.reset_label}
           onClick={handleReset}
         />
-        {searchResults()?.results.length !== 0 && (
-          <p class="fong-medium">
-            {props.t.results_label} "{query()}":{' '}
-            {searchResults()?.results.length}
+        {isQuerying() && (
+          <p class="text-center">
+            {props.t.results_label}
+            <span class="font-medium">
+              {query()}: {searchResults()?.results.length}
+            </span>
           </p>
         )}
       </form>

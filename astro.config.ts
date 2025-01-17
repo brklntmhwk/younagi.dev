a17cf625f94b14b3f6bfee0cbd74c785951b43e6
brklntmhwk_dev <86272619+brklntmhwk@users.noreply.github.com>
2025-01-17 04:40:59 +0000

    fix(config): 🐛 add image service option to cloudflare adapter
    set it to compile so sharp library works with cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import purgecss from 'astro-purgecss';
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig/* , passthroughImageService */ } from 'astro/config';
 import { h } from 'hastscript';
 import rehypeAutolinkHeadings, {
   type Options as RehypeAutoLinkHeadingsOptions,
@@ -44,14 +44,15 @@ import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 export default defineConfig({
   site: SITE_URL,
   adapter: cloudflare({
+    imageService: 'compile',
     platformProxy: {
       enabled: true,
       persist: true,
     },
   }),
-  image: {
-    service: passthroughImageService(),
-  },
+  // image: {
+  //   service: passthroughImageService(),
+  // },
   integrations: [
     expressiveCode({
       themes: ['github-dark', 'catppuccin-latte'],

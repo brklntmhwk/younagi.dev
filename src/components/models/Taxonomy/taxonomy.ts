97b6389e1b9ee94319a10939af7b97c78571b03b
brklntmhwk_dev <86272619+brklntmhwk@users.noreply.github.com>
2025-01-20 06:23:02 +0000

    style(ui): 💄 tweak some styles in pages

--- src/components/models/Taxonomy/taxonomy.ts
+++ src/components/models/Taxonomy/taxonomy.ts
@@ -1,7 +1,7 @@
 import { tv } from 'tailwind-variants';
 
 export const style = tv({
-  base: 'text-default text-center text-xs sm:text-sm hover:bg-default-reverse-hover border-2 border-default border-solid py-0.5 xs:py-1 px-2 xs:px-2.5 py-0.5 xs:py-1 px-2 xs:px-2.5',
+  base: 'text-default text-center text-sm sm:text-base hover:bg-default-reverse-hover border-2 border-default border-solid py-0.5 xs:py-1 px-2 xs:px-2.5 py-0.5 xs:py-1 px-2 xs:px-2.5',
   variants: {
     taxonomy: {
       categories: 'rounded-sm',

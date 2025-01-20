a371a6de623ee4ac135caeaa73f6b159a70331ce
brklntmhwk_dev <86272619+brklntmhwk@users.noreply.github.com>
2025-01-20 05:36:06 +0000

    style(ui): 💄 remove taxonomy colors
    modify content config, taxonomy component, taxonomy data, and front
    matter cms config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
 import { defineCollection, reference, z } from 'astro:content';
-import { colors } from '@/components/models/Taxonomy';
 
 const blog = defineCollection({
   type: 'content',
@@ -34,7 +33,6 @@ const taxonomySchema = z.object({
   title: z.string(),
   slug: z.string(),
   ruby: z.string(),
-  color: z.enum(colors),
 });
 
 const categories = defineCollection({

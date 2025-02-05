eba151f6857dd663661b0e0bdb3c54c2955ba9e7
brklntmhwk <contact@younagi.dev>
2025-02-05 09:03:31 +0000

    refactor(db): remove conditional branch for dev mode

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,25 +1,16 @@
 import type { Config } from 'drizzle-kit';
 
-const { CLOUDFLARE_ACCOUNT_ID, D1_DB_ID, D1_DB_API_TOKEN, LOCAL_DB_PATH } =
-  process.env;
+// These must be set from your Cloudflare dashboard
+const { CLOUDFLARE_ACCOUNT_ID, D1_DB_ID, D1_DB_API_TOKEN } = process.env;
 
-export default LOCAL_DB_PATH
-  ? ({
-      schema: './src/db/schema/index.ts',
-      out: './src/db/migrations',
-      dialect: 'sqlite',
-      dbCredentials: {
-        url: LOCAL_DB_PATH,
-      },
-    } satisfies Config)
-  : ({
-      schema: './src/db/schema/index.ts',
-      out: './src/db/migrations',
-      dialect: 'sqlite',
-      driver: 'd1-http',
-      dbCredentials: {
-        accountId: CLOUDFLARE_ACCOUNT_ID!,
-        databaseId: D1_DB_ID!,
-        token: D1_DB_API_TOKEN!,
-      },
-    } satisfies Config);
+export default {
+  schema: './src/db/schema/index.ts',
+  out: './src/db/migrations',
+  dialect: 'sqlite',
+  driver: 'd1-http',
+  dbCredentials: {
+    accountId: CLOUDFLARE_ACCOUNT_ID!,
+    databaseId: D1_DB_ID!,
+    token: D1_DB_API_TOKEN!,
+  },
+} satisfies Config;

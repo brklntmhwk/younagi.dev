9296b84ab2a1e9633129bb9a6be73273223cd9b8
brklntmhwk <contact@younagi.dev>
2025-02-06 10:00:50 +0000

    fix(ui): modify structure of photo gallery

--- src/components/ui/PhotoGallery/index.ts
+++ src/components/ui/PhotoGallery/index.ts
@@ -1,3 +1,1 @@
 export { default as PhotoGallery } from './PhotoGallery.astro';
-export { default as PhotoItem } from './PhotoItem.astro';
-export { default as PhotoSkelton } from './PhotoSkelton.astro';

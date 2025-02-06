f6001f4c887c019f8a59218a0edf2ce0b99bef56
brklntmhwk <contact@younagi.dev>
2025-02-06 09:40:13 +0000

    feat(api): add photos action for unsplash photos

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,9 @@
 import { form } from './form';
 import { likes } from './likes';
+import { photos } from './photos';
 
 export const server = {
   form,
   likes,
+  photos,
 };
---
+++ src/actions/photos.ts
@@ -0,0 +1,33 @@
+import { ActionError, defineAction } from 'astro:actions';
+import { z } from 'astro:content';
+import { createApi } from 'unsplash-js';
+
+export const photos = {
+  fetch: defineAction({
+    input: z.object({
+      username: z.string(),
+      perPage: z.number().default(6),
+    }),
+    handler: async (input, ctx) => {
+      const accessKey = ctx.locals.runtime.env.UNSPLASH_API_ACCESS_KEY;
+      const apiClient = createApi({ accessKey });
+
+      const { username, perPage } = input;
+
+      const res = await apiClient.users.getPhotos({
+        username,
+        page: 1,
+        perPage,
+      });
+
+      if (res.type === 'error') {
+        throw new ActionError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: `Failed to fetch Unsplash photos: ${res.errors[0]} ${accessKey}`,
+        });
+      }
+
+      return res.response.results;
+    },
+  }),
+};

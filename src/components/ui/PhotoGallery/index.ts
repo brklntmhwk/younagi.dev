f2e491cb3f8b401f0f92d1782ea4fabfd16dfe05
brklntmhwk <contact@younagi.dev>
2025-02-07 00:38:11 +0000

    fix(api): modify structure of photo gallery on homepage

--- src/components/ui/PhotoGallery/index.ts
+++ src/components/ui/PhotoGallery/index.ts
@@ -1,1 +1,3 @@
 export { default as PhotoGallery } from './PhotoGallery.astro';
+export { default as PhotoItem } from './PhotoItem.astro';
+export { default as PhotoSkelton } from './PhotoSkelton.astro';
